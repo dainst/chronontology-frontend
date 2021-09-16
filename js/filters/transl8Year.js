c1ab15d9aaaa8e4b78b4aaa6dff98f6ca037de12
Wolfgang Schmidle <wolfgang.schmidle@dainst.de>
2021-09-16 17:42:11 +0200

    use BCE / CE in dates

--- js/filters/transl8Year.js
+++ js/filters/transl8Year.js
@@ -1,5 +1,18 @@
 'use strict';
 
+// language-specific notation of dates (so far only years):
+// year 1 to the present: 1950 becomes 1950 CE
+// year -9999 to year -1: -776 becomes 776 BCE
+// before year -10000: -12.000 becomes 12.000 BCE or 12,000 BCE etc. 
+//
+// CE / BCE is acceptable both in English and German
+// CE is acceptable even for recent dates
+// a year 0 in the data is always wrong; however, the system 
+//    would simply display it as 0 CE
+// we make no case distinction for dates bigger than 10.000, 
+//    i.e. at least than 8000 years into the future
+
+
 angular.module('chronontology.filters')
 
 	.filter('transl8Year', ['language',function(language){
@@ -12,12 +25,19 @@ angular.module('chronontology.filters')
 
 			var num = (typeof nu === 'string' && !isNaN(nu)) ? +nu : nu;
 
-			if (Math.abs(num) < 10000) return num.toString();
+			if (num >= 0) {
+				return num.toString()+" CE";
+			}
+			
+			num = Math.abs(num);
 
-            if (language.currentLanguage()==COMPONENTS_GERMAN_LANG) {
-                return num.toLocaleString(COMPONENTS_GERMAN_LANG+"-DE");
+			if (num < 10000) {
+				return num.toString()+" BCE";
+			}
+			if (language.currentLanguage()==COMPONENTS_GERMAN_LANG) {
+                return num.toLocaleString(COMPONENTS_GERMAN_LANG+"-DE")+" BCE";
             } else {
-                return num.toLocaleString(COMPONENTS_ENGLISH_LANG+"-US");
+                return num.toLocaleString(COMPONENTS_ENGLISH_LANG+"-US")+" BCE";
             }
         };
         filterFunction.$stateful=true;
